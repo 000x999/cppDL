ee2cd9712d168fc38fca0845826b621a2fd970cf
000x999 <cxrnxgxz@gmail.com>
2024-11-22 20:54:04 -0500

    Fixed small errors in Vector operations and completed Vector operation methods for Vec3 and Vec4

--- src/include/Structures.h
+++ src/include/Structures.h
@@ -36,7 +36,7 @@ namespace Vec{
   : m_x(x_in), 
     m_y(y_in){}
   
-  Vec2 operator+(const Vec2& rhs) const{return Vec2(m_x + rhs.x, m_y + rhs.y);}
+  Vec2 operator+(const Vec2& rhs) const{return Vec2(m_x + rhs.m_x, m_y + rhs.m_y);}
 	Vec2& operator+=(const Vec2& rhs){return *this = *this + rhs;}
 	Vec2 operator*(float rhs) const{return Vec2(m_x * rhs, m_y * rhs);}
 	Vec2& operator*=(float rhs){return *this = *this * rhs;}
@@ -48,6 +48,7 @@ namespace Vec{
 	float GetLengthSq() const{return m_x*m_x + m_y*m_y;}
 	Vec2& Normalize(){return *this = GetNormalized();}
 	Vec2 GetNormalized(){const float len = GetLength(); if (len != 0.0f){return *this * (1.0f / len);} return *this;} 
+  T dot(const Vec2& vec2_in){return *this.m_x * vec2_in.m_x + *this.m_y * vec2_in.m_y;}
 };
 
   #define VEC3  
@@ -56,7 +57,25 @@ namespace Vec{
     T m_x; 
     T m_y;
     T m_z;
-  };
+    Vec3(T x_in, T y_in, T z_in)
+     :
+    m_x(x_in),
+    m_y(y_in), 
+    m_z(z_in){}
+  Vec3 operator+(const Vec3& rhs) const{return Vec3(m_x + rhs.m_x, m_y + rhs.m_y, m_z+rhs.m_z);}
+	Vec3& operator+=(const Vec3& rhs){return *this = *this + rhs;}
+	Vec3 operator*(float rhs) const{return Vec3(m_x * rhs, m_y * rhs, m_z * rhs);}
+	Vec3& operator*=(float rhs){return *this = *this * rhs;}
+	Vec3 operator-(const Vec3& rhs) const{return Vec3(m_x - rhs.m_x, m_y - rhs.m_y, m_z - rhs.m_z);}
+	Vec3& operator-=(const Vec3& rhs){return *this = *this - rhs;}
+	float fast_rsqrt(float x) { return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x))); }
+	float fast_sqrt(float x) { return x * fast_rsqrt(x); }
+	float GetLength(){return fast_sqrt(m_x*m_x + m_y*m_y + m_z*m_z);}
+	float GetLengthSq() const{return m_x*m_x + m_y*m_y + m_z*m_z;}
+	Vec3& Normalize(){return *this = GetNormalized();}
+	Vec3 GetNormalized(){const float len = GetLength(); if (len != 0.0f){return *this * (1.0f / len);} return *this;} 
+  T dot(const Vec3& vec3_in){return *this.m_x * vec3_in.m_x + *this.m_y * vec3_in.m_y + *this.m_z * vec3_in.m_z;}  
+};
 
   #define VEC4  
   template <typename T>
@@ -65,6 +84,25 @@ namespace Vec{
     T m_y;
     T m_z;
     T m_w;
+    Vec4(T x_in, T y_in, T z_in, T w_in)
+     :
+    m_x(x_in),
+    m_y(y_in), 
+    m_z(z_in),
+    m_w(w_in){}
+  Vec4 operator+(const Vec4& rhs) const{return Vec4(m_x + rhs.m_x, m_y + rhs.m_y, m_z+rhs.m_z);}
+	Vec4& operator+=(const Vec4& rhs){return *this = *this + rhs;}
+	Vec4 operator*(float rhs) const{return Vec4(m_x * rhs, m_y * rhs, m_z * rhs);}
+	Vec4& operator*=(float rhs){return *this = *this * rhs;}
+	Vec4 operator-(const Vec4& rhs) const{return Vec4(m_x - rhs.m_x, m_y - rhs.m_y, m_z - rhs.m_z, m_w - rhs.m_w );}
+	Vec4& operator-=(const Vec4& rhs){return *this = *this - rhs;}
+	float fast_rsqrt(float x) { return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x))); }
+	float fast_sqrt(float x) { return x * fast_rsqrt(x); }
+	float GetLength(){return fast_sqrt(m_x*m_x + m_y*m_y + m_z*m_z + m_w*m_w);}
+	float GetLengthSq() const{return m_x*m_x + m_y*m_y + m_z*m_z + m_w*m_w;}
+	Vec4& Normalize(){return *this = GetNormalized();}
+	Vec4 GetNormalized(){const float len = GetLength(); if (len != 0.0f){return *this * (1.0f / len);} return *this;} 
+  T dot(const Vec4& vec4_in){return *this.m_x * vec4_in.m_x + *this.m_y * vec4_in.m_y + *this.m_z * vec4_in.m_z + *this.m_w * vec4_in.m_w;}
   };
 
 }

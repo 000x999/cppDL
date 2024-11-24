b56fc7173f0f34415a4aa80cd2655f7afb0189e3
000x999 <cxrnxgxz@gmail.com>
2024-11-24 17:25:13 -0500

    Added Softmax, RELU, Sigmoid, SILU, Mish, Softplus, squareplus, binary step, Mean and SSE, Still need more work and optimization though. Also not currently sure they all work

--- src/include/Functions.h
+++ src/include/Functions.h
@@ -1,15 +1,88 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-namespace Functionss{
+#include <cmath>
+#include <math.h>
+#include <functional>
+#include <numeric>
+#include <algorithm>
+#include <vector>
+
+namespace Functions{
 //Softmax
+template <typename T>
+inline std::vector<T> Softmax(std::vector<T> val_in, T beta){
+  std::vector<T> exponentiated; 
+  std::vector<T> res;
+  for(size_t i = 0; i < val_in.size(); i++){
+    exponentiated.emplace_back(exp(beta*val_in[i]));   
+  }   
+  for(size_t i = 0; i < exponentiated.size(); i++){
+    res.emplace_back(exponentiated[i]/std::reduce(exponentiated.begin(), exponentiated.end()));
+  }
+  return res; 
+}
 //RELU
+template <typename T>
+inline T RELU(T val_in){
+  if(val_in > 0){
+  return (val_in + std::abs(val_in)) * 0.5f;
+  }else{
+    return 0; 
+  }
+}
 //Sigmoid
-//tanh 
-//leaky RELU
+template <typename T>
+inline T Sigmoid(T val_in){
+  return 1.0f / (1.0f + std::expf(-val_in)); 
+}
+//SiLU
+template <typename T>
+inline T SILU(T val_in){
+  return val_in * Sigmoid(val_in);
+}
+//mish
+template <typename T> 
+inline T Mish(T val_in){
+  return val_in*tanh(Softplus(val_in)); 
+}
+//softplus
+template <typename T>
+inline T Softplus(T val_in){
+  return std::log(1+exp(val_in));
+}
+//squareplus 
+template <typename T> 
+inline T Squareplus(T val_in, T hyperparam){
+  return (val_in + sqrtf((val_in*val_in) + hyperparam)) * 0.5f;
+}
 //binary step
-//linear
+template <typename T>
+inline T BinStep(T val_in){
+  if(val_in < 0){
+    //deactivate node
+    return 0; 
+  }else{
+    //activate node;
+    return 1;
+  }
+}
+//Mean
+template <typename T>
+inline T Mean(std::vector<T> val_in){
+  return (std::reduce(val_in.begin(), val_in.end())) / val_in.size();
+}
 //SSE
-//MSE
-//Linear regression
+template <typename T>
+inline T SSE(std::vector<T> val_in){
+  T res; 
+  T mean = Mean(val_in); 
+  for(size_t i =0; i < val_in.size(); i++){
+   res = res + ((val_in[i] - mean)*(val_in[i] - mean)); 
+  }
+  return res; 
+}
+//feed forward
+//backprop
+//gradient descent 
 }
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@ int main() {
   //First param is rank 
   //All the rest are dimension size 
   //If rank = 5 --> {x,x,x,x,x} and each dimension gets it's own size 
-  Tensor::Tensor<float> tensor(10,{3,3,3,3,3,3,3,3,3,3}); 
-  Tensor::Tensor<float> tensor1(10,{3,3,3,3,3,3,3,3,3,3});
+  Tensor::Tensor<float> tensor(5,{10,10,10,10,10}); 
+  Tensor::Tensor<float> tensor1(5,{10,10,10,10,10});
   tensor.FillTensor();
   tensor1.FillTensor();
   std::cout<<"------------- Tensor 1 ---------------"<<std::endl;
@@ -13,9 +13,10 @@ int main() {
   std::cout<<"------------- Tensor 2 ---------------"<<std::endl;
   tensor1.PrintTensor();
   std::cout<<"------------- Tensor Addition ---------------"<<std::endl;
-  Tensor::Tensor<float> tensor3(10,{3,3,3,3,3,3,3,3,3,3});  
+  Tensor::Tensor<float> tensor3(5,{10,10,10,10,10});  
   tensor3.m_data = tensor + tensor1;
   tensor3.PrintTensor();   
+  std::cout<<"------------- Tensor Addition END ---------------\n\n"<<std::endl;
   std::cout<<"------------- Tensor Dot product ---------------"<<std::endl; 
   std::cout<<Tensor::dot(tensor, tensor1)<<std::endl;
   return 0;

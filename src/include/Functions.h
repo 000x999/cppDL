c58e3953d83ec657c52ec85b43ebae2a19465c4c
000x999 <cxrnxgxz@gmail.com>
2024-11-24 19:30:58 -0500

    Fixed functions so they all take in and return vectors of altered params, makes it easier to maintain tensors and avoids loops, although the functiosn themselves loop now they are inline so should speed things up a bit, ideally i'd find a way to avoid loops entirely.

--- src/include/Functions.h
+++ src/include/Functions.h
@@ -23,48 +23,76 @@ inline std::vector<T> Softmax(std::vector<T> val_in, T beta){
 }
 //RELU
 template <typename T>
-inline T RELU(T val_in){
-  if(val_in > 0){
-  return (val_in + std::abs(val_in)) * 0.5f;
-  }else{
-    return 0; 
+inline std::vector<T> RELU(std::vector<T> val_in){
+  std::vector<T> tempVec;
+  for(size_t i = 0; i < val_in.size(); i++){
+    if(val_in[i] > 0){
+    tempVec.emplace_back((val_in[i] + std::abs(val_in[i])) * 0.5f);
+    }else{
+    tempVec.emplace_back(0); 
+   }
   }
+  return tempVec;
 }
 //Sigmoid
 template <typename T>
-inline T Sigmoid(T val_in){
-  return 1.0f / (1.0f + std::expf(-val_in)); 
+inline std::vector<T> Sigmoid(std::vector<T> val_in){
+  std::vector<T> tempVec; 
+  for(auto& val: val_in){tempVec.emplace_back(1.0f / (1.0f + std::expf(-val)));} 
+  return tempVec;
 }
 //SiLU
 template <typename T>
-inline T SILU(T val_in){
-  return val_in * Sigmoid(val_in);
+inline std::vector<T> SILU(std::vector<T> val_in){
+  std::vector<T> tempVec;
+  std::vector<T> sigVec = Sigmoid(val_in);
+  for(size_t i = 0; i < val_in.size(); i++){
+   tempVec.emplace_back(val_in[i] * sigVec[i]);
+  }
+  return tempVec; 
 }
 //mish
 template <typename T> 
-inline T Mish(T val_in){
-  return val_in*tanh(Softplus(val_in)); 
+inline std::vector<T> Mish(std::vector<T> val_in){
+  std::vector<T> tempVec;
+  std::vector<T> softVec = Softplus(val_in);
+  for(size_t i = 0; i < val_in.size(); i++){
+   tempVec.emplace_back(val_in[i]*tanh(softVec[i]));
+  }
+  return tempVec; 
 }
 //softplus
 template <typename T>
-inline T Softplus(T val_in){
-  return std::log(1+exp(val_in));
+inline std::vector<T> Softplus(std::vector<T> val_in){
+  std::vector<T> tempVec; 
+  for(size_t i = 0; i < val_in.size(); i++){
+  tempVec.emplace_back(std::log(1+exp(val_in[i]))); 
+  }
+  return tempVec;
 }
 //squareplus 
 template <typename T> 
-inline T Squareplus(T val_in, T hyperparam){
-  return (val_in + sqrtf((val_in*val_in) + hyperparam)) * 0.5f;
+inline std::vector<T> Squareplus(std::vector<T> val_in, T hyperparam){
+  std::vector<T> tempVec;
+  for(size_t i = 0; i <val_in.size(); i++){
+    tempVec.emplace_back((val_in[i] + sqrtf((val_in[i]*val_in[i]) + hyperparam)) * 0.5f);
+  }
+  return tempVec;
 }
 //binary step
 template <typename T>
-inline T BinStep(T val_in){
-  if(val_in < 0){
-    //deactivate node
-    return 0; 
-  }else{
-    //activate node;
-    return 1;
+inline std::vector<T> BinStep(std::vector<T> val_in){
+  std::vector<T> tempVec; 
+  for(size_t i =0; i < val_in.size(); i++){
+    if(val_in[i] < 0){
+      //deactivate node
+      tempVec.emplace_back(0); 
+   }else{
+      //activate node;
+     tempVec.emplace_back(1);
+   }
   }
+  return tempVec;
 }
 //Mean
 template <typename T>
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,18 @@
 #include "include/Tensor.h"
-
+#include "include/Functions.h"
 int main() {
   //First param is rank 
   //All the rest are dimension size 
   //If rank = 5 --> {x,x,x,x,x} and each dimension gets it's own size 
-  Tensor::Tensor<float> tensor(5,{10,10,10,10,10}); 
-  Tensor::Tensor<float> tensor1(5,{10,10,10,10,10});
+  Tensor::Tensor<float> tensor(3,{2,2,2}); 
   tensor.FillTensor();
-  tensor1.FillTensor();
-  std::cout<<"------------- Tensor 1 ---------------"<<std::endl;
-  tensor.PrintTensor(); 
-  std::cout<<"------------- Tensor 2 ---------------"<<std::endl;
-  tensor1.PrintTensor();
-  std::cout<<"------------- Tensor Addition ---------------"<<std::endl;
-  Tensor::Tensor<float> tensor3(5,{10,10,10,10,10});  
-  tensor3.m_data = tensor + tensor1;
-  tensor3.PrintTensor();   
-  std::cout<<"------------- Tensor Addition END ---------------\n\n"<<std::endl;
-  std::cout<<"------------- Tensor Dot product ---------------"<<std::endl; 
-  std::cout<<Tensor::dot(tensor, tensor1)<<std::endl;
+  std::cout<<"\n=========== OG Tensor ===============\n"; 
+  tensor.PrintTensor();
+  tensor.m_data = Functions::Sigmoid(tensor.m_data); 
+  std::cout<<"\n=========== Sigmoided Tensor ===============\n";
+  tensor.PrintTensor();
+  std::cout<<"\n=========== RELU tensor ===============\n";  
+  tensor.m_data = Functions::RELU(tensor.m_data);
+  tensor.PrintTensor();
   return 0;
 }

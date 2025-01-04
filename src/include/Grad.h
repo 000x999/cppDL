3cf4b4debedb5c20c497ec8124d3e1b2fc0813df
000x999 <cxrnxgxz@gmail.com>
2025-01-04 17:04:25 -0500

    Added the grad.h file, start of the optim and grad implementations

---
+++ src/include/Grad.h
@@ -0,0 +1,21 @@
+#ifndef GRAD_H
+#define GRAD_H 
+#include <vector>
+#include <immintrin.h>
+#include <algorithm>
+#include <math.h>
+#include <memory>
+#include <cstdint> 
+#include <iostream> 
+
+#ifndef GRAD  
+#define GRAD
+  namespace grad{
+           
+  
+    
+
+
+  } 
+#endif
+#endif

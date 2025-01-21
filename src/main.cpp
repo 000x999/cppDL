83e6dea591cbc4db7f3bfe07ab350b791ce4386f
000x999 <cxrnxgxz@gmail.com>
2025-01-20 19:59:54 -0500

    Added sample code to demonstrate tensor instantiation and usage

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,24 @@ int main() {
   //First param is rank 
   //All the rest are dimension size 
   //If rank = 5 --> {x,x,x,x,x} and each dimension gets it's own size 
-  mat::matrix<float> A(10,10); 
-  mat::matrix<float> B(10,10);
-  A.fillMat(); 
-  B.fillMat(); 
-  std::cout<<"\n=========== MAT A ===============\n";
-  A.displayMat();
-  std::cout<<"\n=========== MAT A ===============\n";
-  std::cout<<"\n=========== MAT B ===============\n";
-  B.displayMat(); 
-  std::cout<<"\n=========== MAT B ===============\n";
-  mat::matrix<float> C = A * B;
-  std::cout<<"\n=========== MAT C ===============\n";
-  C.displayMat(); 
-  std::cout<<"\n=========== MAT C ===============\n";
-  mat::matrix<float> D = C + A; 
-  std::cout<<"\n=========== MAT D ===============\n";
-  D.displayMat(); 
-  std::cout<<"\n=========== MAT D ===============\n";
-
   /*
-  Tensor::Tensor<float> tensor(5,{50,50,50,50,50});  
-  tensor.FillTensor();
-  std::cout<<"\n=========== OG Tensor ===============\n"; 
-  tensor.PrintTensor();
-  std::cout<<"\n=========== Tensor Mean ===============\n";
-  std::cout<<Functions::Mean(tensor.m_data);
+  mat::matrix<float> D(10,10); 
+  mat::matrix<float> A(9,9);
+  int** arr; 
+  A.zeros(); 
+  A.displayMat();
+  std::cout<<"\n";
+  mat::matrix<float> C = A.block(3,3,3,3); 
+  C.displayMat();
+  std::cout<< "\n" << C.sum(); 
   */
+  Tensor::Tensor<float> tensor(5,{5,5,5,5,5});  
+  TensorMath::TensorMath<float> ops(tensor);
+  ops.FillTensor();
+  std::cout<<"\n=========== OG Tensor ===============\n"; 
+  ops.PrintTensor();
+  std::cout<<"\n=========== Tensor Mean ===============\n";
+  std::cout<<Functions::Mean(ops.GetData());
+  
   return 0;
 }

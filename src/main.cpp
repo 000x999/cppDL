a17e7b34fbf9870e014281f4f81331d1659be104
000x999 <cxrnxgxz@gmail.com>
2025-01-23 23:49:02 -0500

    Added sample code to demonstrate neural network instantiation, adding layers and activation functions to the layers

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include "include/Tensor.h"
 #include "include/Functions.h"
 #include "include/Structures.h"
-
+#include "include/NeuralNetwork.h"
 int main() {
   //First param is rank 
   //All the rest are dimension size 
   //If rank = 5 --> {x,x,x,x,x} and each dimension gets it's own size 
-  
+  /*
   mat::matrix<float> D(5,5); 
   mat::matrix<float> A(5,5);
   mat::MatOps::MatOps<float> matops(A); 
@@ -20,15 +20,20 @@ int main() {
   matops.displayMat();
   std::cout<<"\n";
   std::cout<< "\n" << matops.sum(); 
-  
-  /*
-  Tensor::Tensor<float> tensor(5,{5,5,5,5,5});  
+  mat::matrix<float> E = mat::MatOps::MatOps<float>::matmul(A,D);  
+  */
+  nn::Network net; 
+  net.connectLayer<nn::Net>(3,5);
+  net.connectLayer<nn::ReLU>();
+  net.connectLayer<nn::Net>(5,1); 
+
+  Tensor::Tensor<float> tensor(5,{10,10,10,10,10});  
   TensorOps::TensorOps<float> ops(tensor);
   ops.FillTensor();
   std::cout<<"\n=========== OG Tensor ===============\n"; 
   ops.PrintTensor();
   std::cout<<"\n=========== Tensor Mean ===============\n";
   std::cout<<Functions::Mean(ops.GetData());
-  */
+  
   return 0;
 }

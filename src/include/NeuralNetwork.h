ec72c08b4a21016df62b6adc93bb0724c976c19a
000x999 <cxrnxgxz@gmail.com>
2025-01-28 02:10:57 -0500

    Completed first iteration implementation of FeedForward, Backward Propagation and update eta methods for linear ANN layers. They still need to be test

--- src/include/NeuralNetwork.h
+++ src/include/NeuralNetwork.h
@@ -14,15 +14,7 @@
 #include "Functions.h"
 
 namespace Neural{
-//Parent base for all derivations of layer
-class layer{ 
-  public:
-  virtual ~layer() = default; 
-  virtual void Forward(const std::vector<float> &inputVals) = 0;
-  virtual void Backwards(const std::vector<float> &targetVals) = 0; 
-  virtual void update (float eta) {}; 
-};
-  
+
 class layerData{
   public:
   size_t inputSize; 
@@ -41,17 +33,51 @@ class layerData{
     grad(output,0.0f){}
 };
 
+//Parent base for all derivations of layer
+class layer{ 
+  public:
+  virtual ~layer() = default; 
+  virtual void Forward(const std::vector<float> &inputVals, layerData &data) = 0;
+  virtual void Backwards(const std::vector<float> &targetVals, const std::vector<float> &gradOut, layerData &data) = 0; 
+  virtual void update (layerData &data, float eta) {}; 
+};
+
 class linear : public layer{
   public:
-  void Forward(const std::vector<float> &inputVals) override{};
-  void Backwards(const std::vector<float> &inputVals) override{};
-  void update(float eta)override{}; 
+  void Forward(const std::vector<float> &inputVals, layerData &data) override{
+    for(size_t i = 0; i < data.outputSize; ++i){
+      data.activations[i] = data.biases[i]; 
+      for(size_t j = 0; j < data.inputSize; ++j){
+        data.activations[i] += data.weights[i * data.inputSize + j] * inputVals[j];
+      }
+    }
+  };
+
+  void Backwards(const std::vector<float> &targetVals, const std::vector<float> &gradOut, layerData &data) override{
+    for(size_t i = 0; i < data.outputSize; ++i){
+      data.grad[i] = gradOut[i]; 
+      for(size_t j = 0; j < data.inputSize; ++j){
+        data.weights[i * data.inputSize + j] -= gradOut[i] * targetVals[j];
+      }
+      data.biases[i] -= gradOut[i];
+    }
+  };
+
+  void update(layerData &data, float eta)override{
+    for(auto &weights : data.weights){
+      weights -= eta; 
+    }
+    for(auto &bias : data.biases){
+      bias -= eta; 
+    }
+  };
+
 };
   
 class ReLU: public layer{
   public:
-  void Forward(const std::vector<float> &inputVals)override{};
-  void Backwards(const std::vector<float> &inputVals)override{};
+  void Forward(const std::vector<float> &inputVals, layerData &data)override{};
+  void Backwards(const std::vector<float> &targetVals, const std::vector<float> &gradOut, layerData &data)override{};
 };
 
 class nn {

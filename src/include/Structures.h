1f8b1555346ee8a2a19939b513429ab430087761
000x999 <cxrnxgxz@gmail.com>
2025-02-24 22:05:13 -0500

    Removed All Vector container implementations because they were stupid and useless, fixed all issues with matrix and MatOps methods. Greatlely improved matmuls through AVX256 instructions and openMP, current benchmarks for my matmul get around 80GFLOPS at FP64 for 4096x4096 matrices... not amazing but it's getting better

--- src/include/Structures.h
+++ src/include/Structures.h
@@ -6,114 +6,11 @@
 #include <iostream>
 #include <immintrin.h>
 
-namespace Vec{
-#define VEC2
-  template <typename T>
-  struct alignas(sizeof(T)*2) Vec2{
-  T m_x; 
-  T m_y;  
-  Vec2(T x_in, T y_in)
-  : m_x(x_in), 
-    m_y(y_in){}
-  };
-  namespace Vec2Ops{  
-  template <typename T>    
-  class Vec2Ops{
-    private: 
-    Vec::Vec2<T> vec2;
-    public:
-    Vec2Ops(Vec::Vec2<T> &vec2)
-      :vec2(vec2){}
-    Vec2Ops<T> operator+(const Vec2Ops<T>& rhs) const{return Vec2(vec2.m_x + rhs.vec2.m_x, vec2.m_y + rhs.vec2.m_y);}
-    Vec2Ops<T>& operator+=(const Vec2Ops<T>& rhs){return *this = *this + rhs;}
-    Vec2Ops<T> operator*(float rhs) const{return Vec2(vec2.m_x * rhs, vec2.m_y * rhs);}
-    Vec2Ops<T>& operator*=(float rhs){return *this = *this * rhs;}
-    Vec2Ops<T> operator-(const Vec2Ops<T>& rhs) const{return Vec2(vec2.m_x - rhs.vec2.m_x, vec2.m_y - rhs.vec2.m_y);}
-    Vec2Ops<T>& operator-=(const Vec2Ops<T>& rhs){return *this = *this - rhs;}
-    float fast_rsqrt(float x) { return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x))); }
-    float fast_sqrt(float x) { return x * fast_rsqrt(x); }
-    float GetLength(){return fast_sqrt(vec2.m_x*vec2.m_x + vec2.m_y*vec2.m_y);}
-    float GetLengthSq() const{return vec2.m_x*vec2.m_x + vec2.m_y*vec2.m_y;}
-    Vec2Ops<T>& Normalize(){return *this = GetNormalized();}
-    Vec2Ops<T> GetNormalized(){const float len = GetLength(); if (len != 0.0f){return *this * (1.0f / len);} return *this;} 
-    T dot(const Vec::Vec2<T>& vec2_in){return *this.m_x * vec2_in.m_x + *this.m_y * vec2_in.m_y;}
-    };
-  };
-  
-#define VEC3  
-  template <typename T>
-  struct alignas(sizeof(T)*3)Vec3{
-    T m_x; 
-    T m_y;
-    T m_z;
-    Vec3(T x_in, T y_in, T z_in)
-     :
-    m_x(x_in),
-    m_y(y_in), 
-    m_z(z_in){}
-  };
-  namespace Vec3Ops{
-  template <typename T>
-  class Vec3Ops{
-    private:
-    Vec::Vec3<T> vec3; 
-    public:
-      Vec3Ops(Vec::Vec3<T> &vec3)
-        :vec3(vec3){}
-      Vec3Ops<T> operator+(const Vec3Ops<T>& rhs) const{return Vec3(vec3.m_x + rhs.vec3.m_x, vec3.m_y + rhs.vec3.m_y, vec3.m_z+rhs.vec3.m_z);}
-      Vec3Ops<T>& operator+=(const Vec3Ops<T>& rhs){return *this = *this + rhs;}
-      Vec3Ops<T> operator*(float rhs) const{return Vec3(vec3.m_x * rhs, vec3.m_y * rhs, vec3.m_z * rhs);}
-      Vec3Ops<T>& operator*=(float rhs){return *this = *this * rhs;}
-      Vec3Ops<T> operator-(const Vec3Ops<T>& rhs) const{return Vec3(vec3.m_x - rhs.vec3.m_x, vec3.m_y - rhs.vec3.m_y, vec3.m_z - rhs.vec3.m_z);}
-      Vec3Ops<T>& operator-=(const Vec3Ops<T>& rhs){return *this = *this - rhs;}
-      float fast_rsqrt(float x) { return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x))); }
-      float fast_sqrt(float x) { return x * fast_rsqrt(x); }
-      float GetLength(){return fast_sqrt(vec3.m_x*vec3.m_x + vec3.m_y*vec3.m_y + vec3.m_z*vec3.m_z);}
-      float GetLengthSq() const{return vec3.m_x*vec3.m_x + vec3.m_y*vec3.m_y + vec3.m_z*vec3.m_z;}
-      Vec3Ops<T>& Normalize(){return *this = GetNormalized();}
-      Vec3Ops<T> GetNormalized(){const float len = GetLength(); if (len != 0.0f){return *this * (1.0f / len);} return *this;} 
-      T dot(const Vec::Vec3<T>& vec3_in){return *this.m_x * vec3_in.m_x + *this.m_y * vec3_in.m_y + *this.m_z * vec3_in.m_z;}   
-  };
-};
-
-  #define VEC4  
-  template <typename T>
-  struct alignas(sizeof(T)*4)Vec4{
-    T m_x; 
-    T m_y;
-    T m_z;
-    T m_w;
-    Vec4(T x_in, T y_in, T z_in, T w_in)
-     :
-    m_x(x_in),
-    m_y(y_in), 
-    m_z(z_in),
-    m_w(w_in){} 
-  };
-  namespace Vec4Ops{
-  template <typename T>
-  class Vec4Ops{
-    private:
-    Vec::Vec4<T> vec4; 
-    public:
-      Vec4Ops(Vec::Vec4<T> &vec4)
-        :vec4(vec4){}
-      Vec4Ops<T> operator+(const Vec4Ops<T>& rhs) const{return Vec4Ops(vec4.m_x + rhs.vec4.m_x, vec4.m_y + rhs.vec4.m_y, vec4.m_z+rhs.vec4.m_z);}
-      Vec4Ops<T>& operator+=(const Vec4Ops<T>& rhs){return *this = *this + rhs;}
-      Vec4Ops<T> operator*(float rhs) const{return Vec4Ops(vec4.m_x * rhs, vec4.m_y * rhs, vec4.m_z * rhs);}
-      Vec4Ops<T>& operator*=(float rhs){return *this = *this * rhs;}
-      Vec4Ops<T> operator-(const Vec4Ops<T>& rhs) const{return Vec4Ops(vec4.m_x - rhs.vec4.m_x, vec4.m_y - rhs.vec4.m_y, vec4.m_z - rhs.vec4.m_z, vec4.m_w - rhs.vec4.m_w );}
-      Vec4Ops<T>& operator-=(const Vec::Vec4<T>& rhs){return *this = *this - rhs;}
-      float fast_rsqrt(float x) { return _mm_cvtss_f32(_mm_rsqrt_ss(_mm_set_ss(x))); }
-      float fast_sqrt(float x) { return x * fast_rsqrt(x); }
-      float GetLength(){return fast_sqrt(vec4.m_x*vec4.m_x + vec4.m_y*vec4.m_y + vec4.m_z*vec4.m_z + vec4.m_w*vec4.m_w);}
-      float GetLengthSq() const{return vec4.m_x*vec4.m_x + vec4.m_y*vec4.m_y + vec4.m_z*vec4.m_z + vec4.m_w*vec4.m_w;}
-      Vec4Ops<T>& Normalize(){return *this = GetNormalized();}
-      Vec4Ops<T> GetNormalized(){const float len = GetLength(); if (len != 0.0f){return *this * (1.0f / len);} return *this;} 
-      T dot(const Vec::Vec4<T>& vec4_in){return *this.m_x * vec4_in.m_x + *this.m_y * vec4_in.m_y + *this.m_z * vec4_in.m_z + *this.m_w * vec4_in.m_w;}
-    };
-  };
-}
+extern "C" int omp_get_thread_num();
+extern "C" int omp_get_num_threads();
+extern "C" int omp_set_dynamic(int x);
+extern "C" int omp_set_num_threads(int y);
+extern "C" int omp_get_max_threads(); 
 
 #define MATRIX
 namespace mat{
@@ -121,180 +18,232 @@ template <typename T>
 struct matrix{
   size_t m_row; 
   size_t m_col;
-  std::vector<std::vector<T>> mat; 
+  __attribute__((aligned(32))) std::vector<std::vector<T>> mat; 
   matrix(size_t row_in, size_t col_in)
         : 
     m_row(row_in), 
     m_col(col_in),
     mat(row_in, std::vector<T>(col_in, 0)) {}
-  };
+};
   
-namespace MatOps{
-  template <typename T>
-  class MatOps{
-    private:
-    mat::matrix<T> mat; 
-    public:
-      MatOps(mat::matrix<T> &mat)
-        :mat(mat){}
-      void displayMat(){
-       std::cout<<"["<<std::endl;
-        for(size_t i = 0; i < mat.m_row; i++){
-          for(size_t j = 0; j < mat.m_col; j++){
-           std::cout << mat.mat[i][j]<<", ";   
-          }
-          std::cout<<""<<std::endl;
-        }
-       std::cout<<" ]";
-      }
+template <typename T>
+class MatOps{
 
-      void fillMat(){
-        static std::random_device rd; 
-        static std::mt19937 gen(rd()); 
-        static std::uniform_real_distribution<float> randVal(0, 1);
-        for(size_t i = 0; i < mat.m_row; i++){
-          for(size_t j = 0; j < mat.m_col; j++){
-          mat.mat[i][j] = randVal(gen);  
-          }
-        }
-      }
+private:
+  int BLOCK_I = 8; 
+  int BLOCK_J = 8; 
+  int BLOCK_K = 8; 
+   __attribute__((aligned(32))) mat::matrix<T> mat; 
 
-      void zeros(){
-        for(size_t i = 0; i < mat.m_row; i++){
-          for(size_t j = 0; j < mat.m_col; j++){
-          mat.mat[i][j] = 0;  
-          }
-        }
+public:
+  MatOps(mat::matrix<T> &mat)
+    :mat(mat){}
+
+  void setBlockSize(const int size_in){
+    BLOCK_I = size_in; 
+    BLOCK_J = size_in; 
+    BLOCK_K = size_in; 
+  }
+
+  void displayMat(){
+    std::cout<<"["<<std::endl;
+    for(size_t i = 0; i < mat.m_row; i++){
+      for(size_t j = 0; j < mat.m_col; j++){
+        std::cout << mat.mat[i][j]<<", ";   
       }
-      
-     static mat::matrix<T> matmul(const mat::matrix<T>&a, const mat::matrix<T> &b){
-        matrix<T> tempMat(a.m_row, b.m_col); 
-        if(a.mat.m_col != b.mat.m_row){
-          std::cout<<"Invalid matrix sizes"<<std::endl;
-        }
-        for(size_t i = 0; i < tempMat.m_row; i++){
-          for(size_t j = 0; j < tempMat.m_col; j++){
-            for(size_t k = 0; k < tempMat.m_col; k++){
-            tempMat.mat[i][j] += a.mat[i][k] * b.mat[k][j];
-            }
-          }
-        }
-        return tempMat;
+      std::cout<<""<<std::endl;
+    }
+   std::cout<<" ]";
+  }
+    
+
+  T GetIndexVal(size_t i, size_t j){return this->mat.mat[j][j];}      
+
+  size_t GetRow(){return this->mat.m_row;}
+
+  size_t GetCol(){return this->mat.m_col;}
+
+  void fillMat(){
+    static std::random_device rd; 
+    static std::mt19937 gen(rd()); 
+    static std::uniform_real_distribution<float> randVal(1, 5);
+    for(size_t i = 0; i < mat.m_row; i++){
+      for(size_t j = 0; j < mat.m_col; j++){
+      mat.mat[i][j] = randVal(gen);  
       }
-      
-      T dot(const matrix<T>&b){
-        T sum = 0; 
-        if(mat.m_col != b.m_col){
-          std::cout<<"Invalid matrix sizes"<<std::endl;
-        }
-        for(size_t i = 0; i < mat.m_row; i++ ){
-          for(size_t j = 0; j < mat.m_col; j++){
-            sum += mat.mat[i][j] * b.mat[i][j]; 
-          }
-        }
-        return sum;
+    }
+  }
+
+  void zeros(){
+    for(size_t i = 0; i < mat.m_row; i++){
+      for(size_t j = 0; j < mat.m_col; j++){
+      mat.mat[i][j] = 1;  
       }
-      
-     mat::matrix<T> block(size_t i,size_t j,size_t p,size_t q){
-         if (i + p > this->mat.m_row || j + q > this->mat.m_col) {
-            throw std::out_of_range("Block indices out of range");
-        }
-        matrix<T> tempMat(p, q);
-        for(size_t a = 0; a < p; ++a){
-          for(size_t b = 0; b < q; ++b){
-            tempMat.mat[a][b] = mat.mat[a+i][b+j]; 
-          }
-        }
-        return tempMat;
-      }
+    }
+  }
      
-      void TP(){
-        matrix<T> tempMat(this->mat.m_row, this->mat.m_col); 
-        for(size_t i = 0; i < this->mat.mat.m_row; i++){
-          for(size_t j = 0; j < this->mat.mat.m_col; j++){
-            tempMat.mat[i][j] = this->mat.mat[i][j]; 
+  T dot(const matrix<T>&b){
+    T sum = 0; 
+    if(mat.m_col != b.m_col){
+      std::cout<<"Invalid matrix sizes"<<std::endl;
+    }
+    for(size_t i = 0; i < mat.m_row; i++ ){
+      for(size_t j = 0; j < mat.m_col; j++){
+        sum += mat.mat[i][j] * b.mat[i][j]; 
+      }
+    }
+    return sum;
+  }
+    
+  mat::matrix<T> block(size_t i,size_t j,size_t p,size_t q){
+    if (i + p > this->mat.m_row || j + q > this->mat.m_col) {
+      throw std::out_of_range("Block indices out of range");
+    }
+    matrix<T> tempMat(p, q);
+    for(size_t a = 0; a < p; ++a){
+      for(size_t b = 0; b < q; ++b){
+        tempMat.mat[a][b] = mat.mat[a+i][b+j]; 
+      }
+    }
+    return tempMat;
+  }
+   
+  void TP(){
+    omp_set_dynamic(0); 
+    omp_set_num_threads(omp_get_max_threads());
+    matrix<T> tempMat(this->mat.m_row, this->mat.m_col); 
+    #pragma omp parallel for
+    for(size_t i = 0; i < this->mat.m_row; i++){
+      for(size_t j = 0; j < this->mat.m_col; j++){
+        tempMat.mat[i][j] = this->mat.mat[i][j]; 
+      }
+    }
+    size_t temp = this->mat.m_row; 
+    this->mat.m_row = this->mat.m_col; 
+    this->mat.m_col = temp;
+    #pragma omp parallel for
+    for(size_t i = 0; i < this->mat.m_row; i++){
+      for(size_t j = 0; j < this->mat.m_col; j++){
+        this->mat.mat[i][j] = tempMat.mat[j][i]; 
+      }
+    }
+    #pragma omp parallel
+    printf("Thread %d out of %d\n", omp_get_thread_num(), omp_get_num_threads());
+  }
+
+  T sum(){
+    T res = 0;
+      /*
+      T res = std::accumulate(mat.begin(), mat.end(),0.0, 
+            [](matrix<T> lhs, const matrix<T>& rhs){
+                              return std::accumulate(rhs.mat.begin(), lhs.mat.end(), lhs);
+                              });*/
+    typename std::vector<std::vector<T>>::iterator row; 
+    typename std::vector<T>::iterator col; 
+    for(row = mat.mat.begin(); row != mat.mat.end(); ++row){
+      for(col = row->begin(); col != row->end(); ++col){
+          res += *col; 
+      }
+    }
+    return res; 
+  }
+
+  MatOps<T> operator*(const MatOps<T> &rhs)const{
+    
+  #define N static_cast<int>(this->mat.m_row)
+     
+   __attribute__((aligned(32))) mat::matrix<float> A = this->mat;  
+   __attribute__((aligned(32))) mat::matrix<float> B = rhs.mat;
+   __attribute__((aligned(32))) mat::matrix<float> C(this->mat.m_row, this->mat.m_col);
+   __attribute__((aligned(32))) mat::matrix<float> C_ref(this->mat.m_row, this->mat.m_col); 
+   
+    omp_set_dynamic(0); 
+    omp_set_num_threads(omp_get_max_threads());
+    #pragma omp parallel for collapse(2)
+      for(int index = 0; index < N; index += BLOCK_I) {
+          for(int jindex = 0; jindex < N; jindex += BLOCK_J) {
+              int iBlockSize = (index + BLOCK_I <= N) ? BLOCK_I : (N - index);
+              int jBlockSize = (jindex + BLOCK_J <= N) ? BLOCK_J : (N - jindex);
+              
+              float cBlock[BLOCK_I][BLOCK_J] __attribute__((aligned(32)));
+
+              for(int itile = 0; itile < iBlockSize; itile++) {
+                  for(int jtile = 0; jtile < jBlockSize; jtile++) {
+                      cBlock[itile][jtile] = 0.0f;
+                  }
+              }
+              for(int kindex = 0; kindex < N; kindex += BLOCK_K) {
+                  int kBlockSize = (kindex + BLOCK_K <= N) ? BLOCK_K : (N - kindex);
+
+                  for(int itile = 0; itile < iBlockSize; itile++) {
+                      int rowA = index + itile;
+                      for(int jtile = 0; jtile < jBlockSize; jtile += 8) {
+                          __m256 sumVec = _mm256_load_ps(&cBlock[itile][jtile]);
+
+                          for(int ktile = 0; ktile < kBlockSize; ktile++) {
+                              int kIndex = kindex + ktile;
+                              __m256 aVal = _mm256_broadcast_ss(&A.mat[rowA][kIndex]);
+                              __m256 bVal = _mm256_load_ps(&B.mat[kIndex][jindex + jtile]);
+
+                              sumVec = _mm256_fmadd_ps(aVal, bVal, sumVec);
+                          }
+                          _mm256_store_ps(&cBlock[itile][jtile], sumVec);
+                      }
+                  }
+              }
+
+              for(int itile = 0; itile < iBlockSize; itile++) {
+                  int rowC = index + itile;
+                  for(int jtile = 0; jtile < jBlockSize; jtile++) {
+                      C.mat[rowC][jindex + jtile] = cBlock[itile][jtile];
+                  }
+              }
           }
-        }
-        size_t temp = this->mat.m_row; 
-        this->mat.m_row = this->mat.m_col; 
-        this->mat.m_col = temp;
-        for(size_t i = 0; i < this->mat.m_row; i++){
-          for(size_t j = 0; j < this->mat.m_col; j++){
-            this->mat.mat[i][j] = tempMat.mat[j][i]; 
-          }
-        }
       }
+    #pragma omp parallel
+    printf("Thread %d out of %d\n", omp_get_thread_num(), omp_get_num_threads());
+    mat::matrix<T> tempMat = C; 
+    MatOps<T> ops(tempMat); 
+    return ops; 
+  }
 
-      T sum(){
-        T res = 0;
-        /*
-        T res = std::accumulate(mat.begin(), mat.end(),0.0, 
-              [](matrix<T> lhs, const matrix<T>& rhs){
-                                return std::accumulate(rhs.mat.begin(), lhs.mat.end(), lhs);
-                                });*/
-        typename std::vector<std::vector<T>>::iterator row; 
-        typename std::vector<T>::iterator col; 
-        for(row = mat.mat.begin(); row != mat.mat.end(); ++row){
-          for(col = row->begin(); col != row->end(); ++col){
-              res += *col; 
-          }
-        }
-        return res; 
-      }
+  MatOps<T>& operator*=(const MatOps<T> &rhs)const{
+    return *this = *this * rhs;
+  }
 
-    MatOps<T> operator*(const MatOps<T> &rhs)const{
-        matrix<T> tempMat(mat.m_row, rhs.mat.m_col); 
-        if(mat.m_col != rhs.mat.m_row){
-          std::cout<<"Invalid matrix sizes"<<std::endl;
-        }
-        for(size_t i = 0; i <tempMat.m_row; i++){
-          for(size_t j = 0; j <tempMat.m_col; j++){
-            for(size_t k = 0; k < mat.m_col; k++){
-            tempMat.mat[i][j] += mat.mat[i][k] * rhs.mat.mat[k][j];
-            }
-          }
-        }
-        return tempMat; 
+  MatOps<T> operator+(const MatOps<T> &rhs)const{
+    matrix<T> tempMat(mat.m_row, mat.m_col); 
+    if(mat.m_row != rhs.mat.m_row && mat.m_col != rhs.mat.m_col){
+      std::cout<<"Invalid matrix sizes"<<std::endl; 
+    }
+    for(size_t i = 0; i < tempMat.m_row; i++){
+      for(size_t j = 0; j < tempMat.m_col; j++){
+        tempMat.mat[i][j] = mat.mat[i][j]+rhs.mat.mat[i][j];  
       }
+    }
+    return tempMat;  
+  }
 
-    MatOps<T>& operator*=(const MatOps<T> &rhs)const{
-        return *this = *this * rhs;
-      }
+  MatOps<T>& operator+=(const MatOps<T> rhs)const{
+    return *this = *this + rhs; 
+  }
 
-    MatOps<T> operator+(const MatOps<T> &rhs)const{
-        matrix<T> tempMat(mat.m_row, mat.m_col); 
-        if(mat.m_row != rhs.mat.m_row && mat.m_col != rhs.mat.m_col){
-          std::cout<<"Invalid matrix sizes"<<std::endl; 
-        }
-        for(size_t i = 0; i < tempMat.m_row; i++){
-          for(size_t j = 0; j < tempMat.m_col; j++){
-            tempMat.mat[i][j] = mat.mat[i][j]+rhs.mat[i][j];  
-          }
-        }
-       return tempMat;  
+  MatOps<T> operator-(const MatOps<T> &rhs)const{
+    matrix<T> tempMat(mat.m_row, mat.m_col); 
+    if(mat.m_row != rhs.mat.m_row && mat.m_col != rhs.mat.m_col){
+      std::cout<<"Invalid matrix sizes"<<std::endl; 
+    }
+    for(size_t i = 0; i < tempMat.m_row; i++){
+      for(size_t j = 0; j < tempMat.m_col; j++){
+        tempMat.mat[i][j] = mat.mat[i][j]-rhs.mat.mat[i][j];  
       }
+    }
+    return tempMat;  
+  }
 
-    MatOps<T>& operator+=(const MatOps<T> rhs)const{
-        return *this = *this + rhs; 
-      }
-
-    MatOps<T> operator-(const MatOps<T> &rhs)const{
-        matrix<T> tempMat(mat.m_row, mat.m_col); 
-        if(mat.m_row != rhs.mat.m_row && mat.m_col != rhs.mat.m_col){
-          std::cout<<"Invalid matrix sizes"<<std::endl; 
-        }
-        for(size_t i = 0; i < tempMat.m_row; i++){
-          for(size_t j = 0; j < tempMat.m_col; j++){
-            tempMat.mat[i][j] = mat.mat[i][j]-rhs.mat[i][j];  
-          }
-        }
-       return tempMat;  
-      }
-
-    MatOps<T>& operator -=(const MatOps<T> rhs)const{
-        return *this = *this - rhs; 
-      }
-  };
- };
-}
+  MatOps<T>& operator -=(const MatOps<T> rhs)const{
+      return *this = *this - rhs; 
+    }
+};
+};
 #endif

e9a0cae3f122e55af0ddddceba81234d0755f1e3
000x999 <cxrnxgxz@gmail.com>
2025-03-22 04:42:26 -0400

    Added some neat preprocessors to enable or disable AVX256 and also added backup methods in case AVX256 isn't defined/Is turned off

--- src/include/Structures.h
+++ src/include/Structures.h
@@ -4,13 +4,20 @@
 #include <vector>
 #include <random>
 #include <iostream>
-#include <immintrin.h>
-
-extern "C" int omp_get_thread_num();
-extern "C" int omp_get_num_threads();
-extern "C" int omp_set_dynamic(int x);
-extern "C" int omp_set_num_threads(int y);
-extern "C" int omp_get_max_threads(); 
+#ifdef USE_VULKAN
+  /*Placing this here since i'm going to be using a custom compute shader pipeline instead of CUDA or ROCm*/
+#endif
+#ifdef USE_OPENGL
+  /*Going to use openGL in case some people use GPU's that don't suport vulkan*/
+#endif
+#ifdef USE_AVX256
+  #include <immintrin.h>
+  extern "C" int omp_get_thread_num();
+  extern "C" int omp_get_num_threads();
+  extern "C" int omp_set_dynamic(int x);
+  extern "C" int omp_set_num_threads(int y);
+  extern "C" int omp_get_max_threads();
+#endif
 
 #define MATRIX
 namespace mat{
@@ -56,11 +63,8 @@ public:
    std::cout<<" ]";
   }
     
-
   T GetIndexVal(size_t i, size_t j){return this->mat.mat[j][j];}      
-
   size_t GetRow(){return this->mat.m_row;}
-
   size_t GetCol(){return this->mat.m_col;}
 
   void fillMat(){
@@ -94,7 +98,8 @@ public:
     }
     return tempMat;
   }
-   
+  
+#if USE_AVX256
   void TP(){
     omp_set_dynamic(0); 
     omp_set_num_threads(omp_get_max_threads());
@@ -117,7 +122,24 @@ public:
     #pragma omp parallel
     printf("Thread %d out of %d\n", omp_get_thread_num(), omp_get_num_threads());
   }
-
+#else
+  void TP(){
+    matrix<T> tempMat(this->mat.m_row, this->mat.m_col); 
+    for(size_t i = 0; i < this->mat.m_row; i++){
+      for(size_t j = 0; j < this->mat.m_col; j++){
+        tempMat.mat[i][j] = this->mat.mat[i][j]; 
+      }
+    }
+    size_t temp = this->mat.m_row; 
+    this->mat.m_row = this->mat.m_col; 
+    this->mat.m_col = temp;
+    for(size_t i = 0; i < this->mat.m_row; i++){
+      for(size_t j = 0; j < this->mat.m_col; j++){
+        this->mat.mat[i][j] = tempMat.mat[j][i]; 
+      }
+    }
+  }
+#endif
   T sum(){
     T res = 0;
       /*
@@ -134,8 +156,10 @@ public:
     }
     return res; 
   }
-
+  
+#if USE_AVX256
   MatOps<T> operator*(const MatOps<T> &rhs)const{
+  std::cout<<"DEBUG: AVX256_MATMUL_STARTED"<<std::endl; 
   #define N static_cast<int>(this->mat.m_row)
    __attribute__((aligned(32))) mat::matrix<float> A = this->mat;  
    __attribute__((aligned(32))) mat::matrix<float> B = rhs.mat;
@@ -176,7 +200,6 @@ public:
                       }
                   }
               }
-
               for(int itile = 0; itile < iBlockSize; itile++) {
                   int rowC = index + itile;
                   for(int jtile = 0; jtile < jBlockSize; jtile++) {
@@ -191,6 +214,22 @@ public:
     MatOps<T> ops(tempMat); 
     return ops; 
   }
+#else
+  /*Even though this only runs on the off chance someone doesn't support AVX256 in this day and age
+   *I don't have to leave it O(2n^3), I'll implement the same cache aware blocking system I did for the AVX version later*/
+  MatOps<T> operator*(const MatOps<T> &rhs) const{
+    MatOps<T> tempMat = rhs;
+    std::cout<<"DEBUG: std_matmul_started"<<std::endl;
+    for(int i = 0; i < rhs.mat.m_row; ++i){
+      for(int j = 0; j < rhs.mat.m_col; ++j){
+        for(int k = 0; k < rhs.mat.m_col; ++k){
+          tempMat.mat.mat[i][j] = rhs.mat.mat[i][k] * rhs.mat.mat[k][j]; 
+        }
+      }
+    }
+    return tempMat;
+  }
+#endif
 
   MatOps<T>& operator*=(const MatOps<T> &rhs)const{
     return *this = *this * rhs;

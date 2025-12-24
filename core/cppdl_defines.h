b021dc362b229879625541268780b378baec1526
000x999 <cxrnxgxz@gmail.com>
2025-12-24 17:35:39 -0500

    changed defines header name to avoid path issues on linux and cmake

---
+++ core/cppdl_defines.h
@@ -0,0 +1,55 @@
+#ifndef DEFINES_H 
+#define DEFINES_H 
+#include <cassert> 
+#include <string_view> 
+#include <string>
+#include <sstream>
+
+#ifdef USE_AVX256
+  #include <immintrin.h>
+  extern "C" int omp_get_thread_num(); 
+  extern "C" int omp_get_num_threads();
+  extern "C" int omp_set_dynamic(int threads);
+  extern "C" int omp_set_num_threads(int threads);
+  extern "C" int omp_get_max_threads();
+#endif
+
+#ifdef USE_BLAS
+  #include "../core/include/CRUSHBLAS_MODULE/core/defines.h"
+  #include "../core/include/CRUSHBLAS_MODULE/core/BLAS/level3/level3.hpp"
+#endif 
+
+#ifdef DEBUG  
+#define DEBUG_THREADS() do {                                \
+     _Pragma("omp parallel")                                \
+      printf("Thread %d out of %d (File: %s, Line: %d)\n",  \
+             omp_get_thread_num(),                          \
+             omp_get_num_threads(),                         \
+             __FILE__, __LINE__);                           \
+  }while(0)
+#endif
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+  #define CRUSH_PLATFORM_WINDOWS 1
+    #ifndef _WIN64 
+      #error "64BIT IS REQUIRED ON WINDOWS"
+    #endif
+  #elif defines(__linux__) || defined(__gnu_linux__)
+    #define CRUSH_PLATFORM_LINUX 1
+  #endif 
+
+#ifdef CPPDL_EXPORT
+  #ifdef _MSC_VER
+    #define CPPDL_API __declspec(dllexport)
+  #else
+    #define CPPDL_API __attribute__((visibility("default")))
+  #endif
+#else
+  #ifdef _MSC_VER
+    #define CPPDL_API __declspec(dllimport)
+  #else
+    #define CPPDL_API
+  #endif
+#endif
+
+#endif   

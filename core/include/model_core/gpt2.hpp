5ea3e9a852ffee42003d9e2d6b5e440d3dd6f822
000x999 <cxrnxgxz@gmail.com>
2026-01-05 01:59:57 -0500

    Added definition for GPT2 inference test

---
+++ core/include/model_core/gpt2.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include "safetensor_core/safetensor_reader.h"
+#include "tensor_core/tensor.hpp"
+#include "neural_memory/nn_memory.hpp"
+#include "attention_core/attention.hpp"
+
+namespace gpt2 {
+constexpr size_t MAX_LAYERS  = 48;
+constexpr size_t MAX_SEQ_LEN = 2048;
+
+struct config {
+  size_t vocab_size;
+  size_t embed_dim;
+  size_t num_heads;
+  size_t num_layers;
+  size_t max_seq_len;
+  float layer_norm_eps;
+};
+
+struct transformer_block {
+  tens::tensor ln1_weight;
+  tens::tensor ln1_bias;
+  tens::tensor ln2_weight;
+  tens::tensor ln2_bias;
+  tens::tensor ffn_fc_weight;
+  tens::tensor ffn_fc_bias;
+  tens::tensor ffn_proj_weight;
+  tens::tensor ffn_proj_bias;
+};
+
+struct model {
+  config cfg;
+  tens::tensor wte;
+  tens::tensor wpe;
+  transformer_block blocks[MAX_LAYERS];
+  atten::multi_head_attention * attentions [MAX_LAYERS];
+  atten::atten_pool           * atten_pools[MAX_LAYERS];
+  tens::tensor ln_f_weight;
+  tens::tensor ln_f_bias;
+  bool initialized;
+};
+
+void         init_model (model* m                                                    );
+bool         load_model (model* m,const char* path,memory::neural_arena& alloc       );
+tens::tensor forward    (model* m,const tens::tensor& tokens,tens::tensor_pool& pool );
+int          argmax     (const tens::tensor& logits                                  );
+void         free_model (model* m                                                    );
+
+}  // namespace gpt2

bd526948dee26f15282c9d8ce3b23a9bcb316977
000x999 <cxrnxgxz@gmail.com>
2026-01-06 03:20:44 -0500

    Transpose fix maybe?

--- core/impl/model_core_impl/gpt2.cpp
+++ core/impl/model_core_impl/gpt2.cpp
@@ -289,13 +289,13 @@ bool load_model(model *m, const char *path, memory::neural_arena &alloc) {
     b->ln1_bias = load_tensor(&sf, name, alloc);
     
     std::snprintf(name, sizeof(name), "h.%zu.attn.c_attn.weight", i);
-    tens::tensor qkv_weight = load_tensor(&sf, name, alloc);
+    tens::tensor qkv_weight = load_linear_transpose(name);
     
     std::snprintf(name, sizeof(name), "h.%zu.attn.c_attn.bias", i);
     tens::tensor qkv_bias = load_tensor(&sf, name, alloc);
     
     std::snprintf(name, sizeof(name), "h.%zu.attn.c_proj.weight", i);
-    tens::tensor attn_proj_weight = load_tensor(&sf, name, alloc);
+    tens::tensor attn_proj_weight = load_linear_transpose(name);
     
     std::snprintf(name, sizeof(name), "h.%zu.attn.c_proj.bias", i);
     tens::tensor attn_proj_bias = load_tensor(&sf, name, alloc);
@@ -303,9 +303,7 @@ bool load_model(model *m, const char *path, memory::neural_arena &alloc) {
     float* w_q_buf = alloc.nn_alloc<float>(embed_dim * embed_dim);
     float* w_k_buf = alloc.nn_alloc<float>(embed_dim * embed_dim);
     float* w_v_buf = alloc.nn_alloc<float>(embed_dim * embed_dim);
-
     float* src_ptr = qkv_weight.tensor_data;
-
     std::memcpy(w_q_buf, src_ptr, embed_dim * embed_dim * sizeof(float));
     std::memcpy(w_k_buf, src_ptr + embed_dim * embed_dim, embed_dim * embed_dim * sizeof(float));
     std::memcpy(w_v_buf, src_ptr + 2 * embed_dim * embed_dim, embed_dim * embed_dim * sizeof(float));
@@ -330,13 +328,13 @@ bool load_model(model *m, const char *path, memory::neural_arena &alloc) {
     b->ln2_bias = load_tensor(&sf, name, alloc);
     
     std::snprintf(name, sizeof(name), "h.%zu.mlp.c_fc.weight", i);
-    b->ffn_fc_weight = load_tensor(&sf, name, alloc); 
+    b->ffn_fc_weight = load_linear_transpose(name); 
     
     std::snprintf(name, sizeof(name), "h.%zu.mlp.c_fc.bias", i);
     b->ffn_fc_bias = load_tensor(&sf, name, alloc);
     
     std::snprintf(name, sizeof(name), "h.%zu.mlp.c_proj.weight", i);
-    b->ffn_proj_weight = load_tensor(&sf, name, alloc); 
+    b->ffn_proj_weight = load_linear_transpose(name); 
     
     std::snprintf(name, sizeof(name), "h.%zu.mlp.c_proj.bias", i);
     b->ffn_proj_bias = load_tensor(&sf, name, alloc);

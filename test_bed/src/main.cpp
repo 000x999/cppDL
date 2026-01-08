43b78e53e7f71c2370d2c0a3dc2444b851f5b327
000x999 <cxrnxgxz@gmail.com>
2026-01-08 03:20:37 -0500

    Doing some gemm_tests against OpenBLAS and oneMKL

--- test_bed/src/main.cpp
+++ test_bed/src/main.cpp
@@ -386,6 +386,7 @@ void gemm_test(float A){
 }
 
 int main(int argc, char* argv[]) {
+/*
   const char* model_path = "model.safetensors";
   const char* vocab_path = "vocab.json";
   const char* merges_path = "merges.txt";
@@ -495,4 +496,8 @@ int main(int argc, char* argv[]) {
   std::free(sequence);
   gpt2::free_model(&model);
   return 0;
+  */
+
+  gemm_test(1024);
+  return 0; 
 }
